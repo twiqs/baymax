7266b528833c09d65e6724ec5de071cec7bd5da3
Adelina <adelinaakylbekova565@gmail.com>
2024-08-16 01:35:07 +0600

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,141 @@
+import VolunteerInfo from "@/components/layout/volunteerInfo";
+import Image from "next/image";
+import Hero from "@/components/layout/Hero";
+
 export default function About() {
-    return(
+    return (
+
         <div className="about-wrap">
+            <Hero></Hero>
             <div className="container">
-                
+                <div class="costumer_cards">
+                    <div className="founders 
+                ">
+                        <h2 className="found-tit">Our founders</h2>
+                        <div className="founders-wrap flex">
+                            <div class="costumer_card costumer_card__1 ">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Jasmin Ibragimova</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">To learn a new language is to open another window from which to see the world.</p>
+                            </div>
+                            <div class="costumer_card costumer_card__2">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Sezim Aktan </h3>
+                                <p class="costumer_status"> -Founder and CEO of the educational organization “Beymax English”</p>
+                                <p class="costumer_review"></p>
+                            </div>
+                            <div class="costumer_card costumer_card__3">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Baktygul Jyldyzbek kyzy</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">Do not squander time - this is stuff life is made of.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="founders 
+                ">
+                        <h2 className="found-tit">Our volunteers</h2>
+                        <div className="founders-wrap flex">
+                            <div class="costumer_card costumer_card__ ">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Jasmin Ibragimova</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">To learn a new language is to open another window from which to see the world.</p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Sezim Aktan </h3>
+                                <p class="costumer_status"> -Founder and CEO of the educational organization “Beymax English”</p>
+                                <p class="costumer_review"></p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Baktygul Jyldyzbek kyzy</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">Do not squander time - this is stuff life is made of.</p>
+                            </div>
+                        </div>
+                        <div className="founders-wrap flex">
+                            <div class="costumer_card costumer_card__ ">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Jasmin Ibragimova</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">To learn a new language is to open another window from which to see the world.</p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Sezim Aktan </h3>
+                                <p class="costumer_status"> -Founder and CEO of the educational organization “Beymax English”</p>
+                                <p class="costumer_review"></p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Baktygul Jyldyzbek kyzy</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">Do not squander time - this is stuff life is made of.</p>
+                            </div>
+                        </div>
+                        <div className="founders-wrap flex">
+                            <div class="costumer_card costumer_card__ ">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Jasmin Ibragimova</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">To learn a new language is to open another window from which to see the world.</p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Sezim Aktan </h3>
+                                <p class="costumer_status"> -Founder and CEO of the educational organization “Beymax English”</p>
+                                <p class="costumer_review"></p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Baktygul Jyldyzbek kyzy</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">Do not squander time - this is stuff life is made of.</p>
+                            </div>
+                        </div>
+                        <div className="founders-wrap flex">
+                            <div class="costumer_card costumer_card__ ">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Jasmin Ibragimova</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">To learn a new language is to open another window from which to see the world.</p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Sezim Aktan </h3>
+                                <p class="costumer_status"> -Founder and CEO of the educational organization “Beymax English”</p>
+                                <p class="costumer_review"></p>
+                            </div>
+                            <div class="costumer_card costumer_card__">
+
+                                <i class="costumer_ic"></i>
+                                <h3 class="costumer_name">Baktygul Jyldyzbek kyzy</h3>
+                                <p class="costumer_status">Co-founder of the educational organization "Baymax English"</p>
+                                <p class="costumer_review">Do not squander time - this is stuff life is made of.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         </div>
     )
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -7,8 +7,10 @@ export default function Hero() {
             <div class="container">
                 <div class="main_info flex">
                     <div class="main_info_info">
-                        <h2 class="main_tit">Welcome to Baymax English, your premier destination for free English learning!</h2>
-                        <p class="sec_txt main_txt"></p>
+                        <h2 class="main_tit">Our people <br /> Meet our Squad</h2>
+                        <p class="sec_txt main_txt">Our team may be small, but our reach is big. The Girl Up staff is innovative, experienced, and passionate. We are deeply committed to working towards a world where all girls are empowered!
+
+</p>
                         <button class="transition section_btn main_btn animation">Podrobnee</button>
 
                     </div>
